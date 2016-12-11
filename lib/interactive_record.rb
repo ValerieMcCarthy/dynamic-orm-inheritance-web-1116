1b657b66d1ce612fe1b2fe419060b0706291818a
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-12-11 09:10:28 -0500

    Readme done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,7 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+#parent classs
 class InteractiveRecord
 
   def self.table_name
--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
 require_relative "./interactive_record.rb"
 
+#class Song inherit from parent super class
+# all methods in parent class available to song
 class Song < InteractiveRecord
 
   self.column_names.each do |col_name|
